0ca81cdb7b2f1e940e9f069687459401cc78d635
gajsingh <gajsingh@publicisgroupe.net>
2023-06-19 17:39:26 +0530

    Removed Typescript from the turborepo

---
+++ packages/ui/Button.js
@@ -0,0 +1,7 @@
+"use client";
+
+import * as React from "react";
+
+export const Button = () => {
+  return <button onClick={() => alert("You clicked a common ui component - Button")}>Click Me</button>;
+};
---
+++ packages/ui/Header.js
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export const Header = ({ text }) => {
+  return <h1>{text}</h1>;
+};
